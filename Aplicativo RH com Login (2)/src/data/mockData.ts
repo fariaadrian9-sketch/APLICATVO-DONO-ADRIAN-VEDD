8f0b64f68af10a3f412eea032b5b876340ebfa0c
fariaadrian9-sketch <fariaadrian9@gmail.com>
2026-09-04 15:55:29 -0300

    Adicionado login

--- Aplicativo RH com Login (2)/src/data/mockData.ts
+++ Aplicativo RH com Login (2)/src/data/mockData.ts
@@ -6,6 +6,10 @@ export const initialAdmins: Admin[] = [
     name: 'Administrador',
     login: '14027455916',
     password: '45032010',
+    ID: 'funcionario adrian'
+    name:'funcionario'
+    login:Adrian@
+    password:123456
   },
 ];
 
